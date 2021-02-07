a17d0f92e4f3a46df74bd9ca5be2fdbb45279d89
GenericMale <genericmale00@gmail.com>
2021-02-07 14:58:10 +0100

    add min and max sizes for emojis

--- src/commands/big.command.ts
+++ src/commands/big.command.ts
@@ -7,6 +7,9 @@ import {GuildMember} from 'discord.js';
 const CUSTOM_EMOJI_REGEX = /<(a)?:(.+):(\d+)>/;
 const BUILTIN_EMOJI_REGEX = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/;
 
+const MIN_SIZE = 22;
+const MAX_SIZE = 300;
+
 export class BigCommand extends Command {
 
     interaction = {
@@ -50,7 +53,7 @@ export class BigCommand extends Command {
         if (options.user) {
             return this.executeUser(author, options.user.user);
         } else if (options.emoji) {
-            return this.executeEmoji(options.emoji.emoji, options.emoji.size | 128);
+            return this.executeEmoji(options.emoji.emoji, options.emoji.size || 128);
         }
     }
 
@@ -81,12 +84,13 @@ export class BigCommand extends Command {
             const response = await fetch(`https://raw.githubusercontent.com/twitter/twemoji/gh-pages/v/13.0.1/svg/${builtIn}.svg`);
             const body = await response.buffer();
 
-            const svg = sharp(body, {density: emojiSize * 2})
-                .resize(emojiSize).toFormat('png').png({quality: 100});
+            const size = Math.max(MIN_SIZE, Math.min(MAX_SIZE, emojiSize));
+            const svg = sharp(body, {density: size * 2})
+                .resize(size).toFormat('png').png({quality: 100});
             const buffer = await svg.toBuffer();
             return {
                 image: {
-                    url: `attachment://emoji.png`
+                    url: `attachment://emoji.png`,
                 },
                 files: [{
                     attachment: buffer,
